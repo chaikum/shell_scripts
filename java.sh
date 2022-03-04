95d88a971a15eb597fb1675a6fe09d76e978de2f
chaikum <chaithanyakumar375@gmail.com>
2022-03-04 17:05:57 +0000

    java shell script changed

--- java.sh
+++ java.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
-sudo su -
-apt update
-if [ javac -version -eq "javac 1.8.0_312"]
-then
+if [ "$(javac -version)" == "javac 1.8.0_312" ];then
     echo "The java is installed"
-else
- 
-    apt install openjdk-8-jdk -y
+  else
+    sudo apt-get update -y
+    sudo apt install openjdk-8-jdk -y
+    javac -version
 fi
-javac -version
